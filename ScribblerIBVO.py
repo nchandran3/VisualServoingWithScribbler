de9313583be10850c867285f963a1722817a0bed
Naveen Chandran <naveenkchandran@gmail.com>
2015-04-14 19:54:48 -0400

    ScribblerIBVO Class extends functionality of Scribbler2 class and implements methods that employ Image Based Visual Servoing

---
+++ ScribblerIBVO.py
@@ -0,0 +1,13 @@
+"""
+@author Naveen Chandran, Justin Jackson, Christian Nash
+This robot class essentially controls the robot and uses Visual Servoing to navigate between two different
+locations that are given through images. It calculates its pose at each succession of the algorithm and compares it
+to the ending result. If they are within a certain threshold, it returns successful and stops all movement. 
+"""
+from Scribbler2 import Scribbler2
+
+class ScribblerIBVO(Scribbler2):
+    
+    def __init___(self):
+        pass
+    
