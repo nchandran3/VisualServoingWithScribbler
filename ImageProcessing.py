05b9c9d962535b5c7cc8d00f0301908d2000d94f
Naveen Chandran <naveenkchandran@gmail.com>
2015-04-14 20:38:53 -0400

    ImageProcessing class added - contains all image based processing such as identifying pixels and making inferences.

---
+++ ImageProcessing.py
@@ -0,0 +1,32 @@
+"""
+Used for testing image processing functionality for the ScribblerIBVO Class
+@author Naveen Chandran
+"""
+
+class ImageProcessing():
+    
+
+    def getBlackBox(self, img):
+        pass
+
+
+
+
+
+
+
+
+
+
+def main(args):
+    img = args[0]
+    with open(img) as f:
+        print f.read()
+        
+        
+        
+        
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
+        
