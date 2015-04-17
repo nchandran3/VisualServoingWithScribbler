3b08a73b9b9907a534ccbf616c042c025f4f9973
Naveen Chandran <naveenkchandran@gmail.com>
2015-04-16 22:23:20 -0400

    Updates

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -3,7 +3,9 @@ Used for testing image processing functionality for the ScribblerIBVO Class
 @author Naveen Chandran
 """
 from PIL import Image
-import cv2 as cv
+import cv2
+import numpy as np
+from cv2 import drawContours
 
 class ImageProcessing():
     
@@ -12,14 +14,14 @@ class ImageProcessing():
         pass
     
     """
+    **METHOD NOT USED** Because most stuff is happening with CV2
     Converts and image to black and white (not grayscale) from a color image.
-    @param img: The relative file path of the image
+    @param img: A PIL Image object
     @return    An image object representing the color picture in black and white
     """
     @staticmethod
     def imgToBlackAndWhite(img):
-        color_img = Image.open(img)
-        gray_img = color_img.convert('L')
+        gray_img = img.convert('L')
         bw_img = gray_img.point(lambda x: 1 if x>50 else 0, '1')
         return bw_img
 
@@ -27,30 +29,58 @@ class ImageProcessing():
 
     """
     Crops the image the specified percent from the left, right, top, and bottom
-    @param img:    The image 
+    @param img:    The PIL image object
     @param percent:     (double) The percent to crop from all sides from 0 - 0.5
+    @param cv2:         True if using cv2 image manipulation, False if using PIL 
     """
     @staticmethod
-    def cropFromSides(img, percent):
-        width, height = img.size
-        left, upper, right, bottom = (int(width*percent), int(height*percent), int(width*(1-percent)), int(height*(1-percent)))
-        return img.crop((left,upper,right,bottom))
-        
-
+    def cropFromSides(img, percent, cv2 = True):
+        if cv2:
+            height, width = img.shape[:2]
+            left, upper, right, bottom = (int(width*percent), int(height*percent), int(width*(1-percent)), int(height*(1-percent)))
+            return img[upper:bottom, left:right] 
+            
+        else:
+            width, height = img.size
+            left, upper, right, bottom = (int(width*percent), int(height*percent), int(width*(1-percent)), int(height*(1-percent)))
+            return img.crop((left,upper,right,bottom))
+            
+    
 
 def main(args):
     #set args values here
-    img = args[0]
+    img_path = args[0]
     
     """
     Begin debugging code here
     """
- 
+    
+    img_raw = cv2.imread(img_path)
+    img = ImageProcessing.cropFromSides(img_raw, .15)   #crop out unnecessary space
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)     #convert to grayscale
+    ret,thresh = cv2.threshold(img_gray,30,255,0)       #convert to binary (black and white)
+    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #find contours
+    cv2.drawContours(img,contours,-1,(0,255,0),1)   #draw contours on cropped image
+    
+    cv2.imshow("Contours",img_raw)      #display contours on normal image
+    cv2.waitKey()
+    
+    epsilon = 0.1*cv2.arcLength(contours, True)
+    img2 = img.clone()
+    
+    for cnt in contours:
+        approx = cv2.approxPolyDP(contours, epsilon, True)
+        if len(approx == 4):
+            break
         
-        
-        
+    cv2.drawContours(img2, approx, -1, (0,255,0), 1)
+    cv2.imshow("Approximates", img2)
+    cv2.waitKey()
+    
         
 if __name__ == "__main__":
     import sys
-    main(sys.argv[1:])
+    import Square
+    print Square.get_axis(sys.argv[1])
+    #main(sys.argv[1:])
         
---
+++ Square.py
@@ -0,0 +1,85 @@
+'''
+Simple "Square Detector" program.
+Loads several images sequentially and tries to find squares in each image.
+'''
+
+import numpy as np
+import cv2
+import math
+
+def angle_cos(p0, p1, p2):
+    d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+    return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
+
+def find_squares(img):
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    squares = []
+    for gray in cv2.split(img):
+        for thrs in xrange(0, 255, 26):
+            if thrs == 0:
+                bin = cv2.Canny(gray, 0, 50, apertureSize=5)
+                bin = cv2.dilate(bin, None)
+            else:
+                retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
+            contours, hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+            for cnt in contours:
+                cnt_len = cv2.arcLength(cnt, True)
+                cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+                if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+                    cnt = cnt.reshape(-1, 2)
+                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
+                    if max_cos < 0.1:
+                        squares.append(cnt)
+    return squares
+
+def get_centroid(square):
+    # GET LINES
+    i = 0
+    lines = []
+    for i in range(len(square)-1):
+        lines.append(((square[i][0], square[i][1]), (square[i+1][0], square[i+1][1])))
+    lines.append(((square[0][0], square[0][1]),(square[len(square)-1][0], square[len(square)-1][1])))
+
+    #print lines
+
+    # GET CENTROID LINE
+    midpoints = []
+    for line in lines:
+        midpoints.append(( (line[0][0]+line[1][0])/2, (line[0][1]+line[1][1])/2 ))
+
+    #find comparable x coords in midpoints
+    threshold = 8
+    centroid = None
+    for mid1 in midpoints:
+        for mid2 in midpoints:
+                if mid1 != mid2 and np.abs(mid1[0] - mid2[0]) < threshold:
+                    centroid = (mid1, mid2)
+                    break
+
+    return centroid
+
+def get_axis(img):
+    from glob import glob
+
+    img = cv2.imread(img)
+    squares = find_squares(img)
+
+    smallest = None
+    sarea = 10000000
+    for square in squares:
+        if cv2.contourArea(square) < sarea:
+            smallest = square
+            sarea = cv2.contourArea(square)
+
+    line = get_centroid(square)
+    line = [np.asarray(line)]
+
+    squares = [square]
+    cv2.drawContours( img, squares, -1, (0, 255, 0), 1 )
+    cv2.drawContours( img, line, -1, (255, 0, 0), 1 )
+    cv2.imshow('squares', img)
+    ch = 0xFF & cv2.waitKey()
+
+    cv2.destroyAllWindows()
+    return line
